0fd076bcf77f3f5cf3bf7abca30b89facab23d3a
sizaif <sizaif2000@gmail.com>
2021-12-22 16:35:16 +0800

    update to LTEStateLearner

--- src/main/java/nl/cypherpunk/statelearner/Sut/sul/Process/ProcessLaunchTrigger.java
+++
@@ -1,9 +0,0 @@
-package nl.cypherpunk.statelearner.Sut.sul.Process;
-
-/**
- * When should a process be (re-)started. In case of a restart, the previous
- * process is terminated.
- */
-public enum ProcessLaunchTrigger {
-	START, NEW_TEST
-}
